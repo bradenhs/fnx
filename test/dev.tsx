dd654a795cecbfd8e675a997f78e95d71901039c
Braden Snell <bhsnll@gmail.com>
2017-03-21 13:50:26 -0600

    remveos bad test

--- test/dev.tsx
+++ test/dev.tsx
@@ -1,38 +1,6 @@
-import * as React from 'react'
-import * as renderer from 'react-test-renderer'
-import ReactiveComponent from '../src/extras/react'
-import { action, createObservable, string } from '../src/fnx'
-
 /**
  * Use this file for constructing new test suites without having to worry about
  * running all the tests every single time.
  *
  * Run with `yarn run dev`
  */
-
-it('asdf', () => {
-  class State {
-    name = string
-    changeName? = action((s: State) => (name: string) => {
-      s.name = name
-    })
-  }
-
-  const state = createObservable(State, { name: '' })
-
-  let runs = 0
-
-  const Component = ReactiveComponent(() => {
-    runs++
-    return <div/>
-  })
-
-  renderer.create(<Component/>)
-
-  state.changeName('Bob')
-
-  const actual = runs
-  const expected = 2
-
-  expect(actual).toBe(expected)
-})
